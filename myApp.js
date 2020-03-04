046579cc02b2494cd0cea5e69f238b873766cefa
peilli <19546253+ppeill@users.noreply.github.com>
2020-03-04 13:10:43 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,20 +1,37 @@
+
+/** luminous-silk-helmetjs-exercises **/
+
 const helmet = require("helmet");
-const hidePoweredBy = require ('hide-powered-by');
-const frameguard = require ('frameguard');
+const hidePoweredBy = require("hide-powered-by");
+const frameguard = require("frameguard");
+const xssFilter = require("x-xss-protection");
+const noSniff = require("dont-sniff-mimetype");
+const ieNoOoen = require("ienoopen");
+const hsts = require("hsts");
+const ninetyDaysInMilliseconds = 7776000;
+
 var express = require("express"); // Do Not Edit
 var app = express(); // Do Not Edit
 
-// ----
-
-/** - Challenges - *
-
-
+// 
 
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.xssFilter());
 
+app.use(noSniff());
+app.use(helmet.ieNoOpen());
 
 var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+//var ninetyDaysInMilliseconds = 90 * 24 * 60 * 60 * 1000;
+
+app.use(helmet.hsts({ maxAge: ninetyDaysInMilliseconds, force: true }));
+
+
+
+
+
+
 
 
 // ---- DO NOT EDIT BELOW THIS LINE ---------------------------------------
@@ -29,3 +46,4 @@ app.get("/", function(request, response) {
 });
 var listener = app.listen(process.env.PORT || 3000, function() {
   console.log("Your app is listening on port " + listener.address().port);
+});
